a38e20775988395d359d1296efc81edc71ad2791
Luan Lopes <luanglopes@gmail.com>
2022-02-03 17:02:17 -0300

    use resolver factory to avoid duplicated code

--- transpile-only.js
+++ transpile-only.js
@@ -1,15 +1,6 @@
 import { resolve as resolveTs } from 'ts-node/esm/transpile-only'
-import * as tsConfigPaths from 'tsconfig-paths'
-import { pathToFileURL } from 'url'
+import { resolverFactory } from './resolverFactory'
 
-const { absoluteBaseUrl, paths } = tsConfigPaths.loadConfig()
-const matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths)
-
-export function resolve(specifier, ctx, defaultResolve) {
-  const match = matchPath(specifier)
-  return match
-    ? resolveTs(pathToFileURL(`${match}`).href, ctx, defaultResolve)
-    : resolveTs(specifier, ctx, defaultResolve)
-}
+export const resolve = resolverFactory(resolveTs)
 
 export { getFormat, load, transformSource } from 'ts-node/esm/transpile-only'
